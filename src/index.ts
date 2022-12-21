a23553bdaf02ded6fae4c0496ad72bbd93f890e9
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-12-21 21:57:22 +0500

    diff files routing

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import * as dotenv from "dotenv";
 import connectToMongoDB from "./config/db.js";
 import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
-// import { router } from "./routes/users.js";
+import { router } from "./routes/users.js";
 dotenv.config();
 const _port: string | undefined = process.env.PORT;
 const app: Application = express();
 connectToMongoDB();
 //  middle wares
+app.use(express.json());
 app.use(cors());
-// app.use("/auth", router);
-app.get("/", () => {
-  console.log("Abu hurairah");
-});
+app.use("/auth", router);
 // Checking if the server is successfully started or not
 app.listen(_port, () => {
   console.log(`Server started working on port is working on ${_port}`);
