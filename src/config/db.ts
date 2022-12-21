0b3dc2947c0166969c47eeb277a1e48c4c03c7fa
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-12-21 17:45:29 +0500

    env file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-require("dotenv").config("./../../.env");
+import * as dotenv from "dotenv";
+dotenv.config();
 const port1: string | undefined = process.env.PORT;
-console.log("🚀 ~ file: db.ts:4 ~ port", port1);
 export default port1;
-
 // const connectToMongoDB = () => {
 //   mongoose.connect(port, () => {
 //     console.log("MongoDB is working Properly");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as dotenv from "dotenv";
+import port1 from "./config/db.js";
+console.log("🚀 ~ file: index.ts:3 ~ port1", port1);
 dotenv.config();
 const port: string | undefined = process.env.PORT;
 console.log(port);
